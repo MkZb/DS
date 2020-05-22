adb77c126718cf0ab8e58ba4e19ca68df1bdff8b
Kolya <kolya.zubets@gmail.com>
2020-05-22 12:15:13 +0300

    Small update

---
+++ public/javascripts/finder.js
@@ -0,0 +1,11 @@
+function FindPlayer(){
+	var name = document.getElementById('getname').value
+	document.getElementById("Sraka").innerHTML = name
+	document.getElementById("head").innerHTML = "PLAYER SEARCH RESULT"
+}
+
+function FindMatch(){
+	var name = document.getElementById('getnum').value
+	document.getElementById("Sraka").innerHTML = name
+	document.getElementById("head").innerHTML = "MATCH SEARCH RESULT"
+}
