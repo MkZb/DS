d65028c9faa4ba2aeb1a91f381bcc63f9d0da2d0
Kolya <kolya.zubets@gmail.com>
2020-05-22 12:01:25 +0300

    Small update

--- routes/graphics.js
+++ routes/graphics.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendFile(path.join(__dirname, '../', '/public/graphics.html'));
 });
 
 module.exports = router;
