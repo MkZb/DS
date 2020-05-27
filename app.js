a71273eb336f7f86a46af0d198950f71bac6af72
Kolya <kolya.zubets@gmail.com>
2020-05-27 13:02:13 +0300

    Added testing tools to dependencies.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/graphics');
+var graphicsRouter = require('./routes/graphics');
+var finderRouter = require('./routes/finder');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/graphics', usersRouter);
+app.use('/graphics', graphicsRouter);
+app.use('/finder', finderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/finder.js
+++ routes/finder.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-/* GET graphics listing. */
+/* GET finder page. */
+
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../', '/public/graphics.html'));
+    res.sendFile(path.join(__dirname, '../', '/public/finder.html'));
 });
 
 module.exports = router;
