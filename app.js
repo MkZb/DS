fea715420a350a366673f37cde31460ac3e9c712
Kolya <kolya.zubets@gmail.com>
2020-05-28 12:06:06 +0300

    Added hero statistics!

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var graphicsRouter = require('./routes/graphics');
 var finderRouter = require('./routes/finder');
+var heroesRouter = require('./routes/heroeinfo');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/graphics', graphicsRouter);
 app.use('/finder', finderRouter);
+app.use('/heroinfo.html', heroesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
